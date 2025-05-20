d721b4642d2ac378e2566f3cb89496fc137f3017
Amiral30 <almirantevianney2005@gmail.com>
2025-05-20 19:34:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // POSITION (doit correspondre au CSS)
         photoPosition: {
             x: 50,  // Ne pas changer (centrage horizontal)
-            y: 100   // ← Même valeur que --photo-position-y
+            y: 10   // ← Même valeur que --photo-position-y
         }
     };
 
